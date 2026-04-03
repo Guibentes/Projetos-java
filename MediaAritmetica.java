48158d0a15279839cce53eb08207c874f933447d
Guilherme Bentes Teixeira Xavier <142184820+Guibentes@users.noreply.github.com>
2026-04-03 17:57:59 -0300

    Add files via upload

---
+++ MediaAritmetica.java
@@ -0,0 +1,35 @@
+package exercicios_java;
+
+import java.util.Scanner;
+
+
+public class MediaAritmetica {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double nota1, nota2, nota3, nota4;
+		double media;
+		
+		
+		
+		
+		System.out.println("Digite sua nota:");
+		nota1 = scanner.nextDouble();
+		System.out.println("Digite sua nota:");
+		nota2 = scanner.nextDouble();
+		System.out.println("Digite sua nota:");
+		nota3 = scanner.nextDouble();
+		System.out.println("Digite sua nota:");
+		nota4 = scanner.nextDouble();
+		
+		
+		
+		media = (nota1 + nota2 + nota3 + nota4)/4; 
+		
+		System.out.println("Sua media foi: " + media);
+		
+		scanner.close();
+	}
+}

c0fdf03a7ac8252bc141bdee7483e63b3850d65f
Guibentes <guilhermebentes2006@gmail.com>
2026-04-03 15:58:30 -0300

    Corrigido programa de conversão de temperatura F para C com input do usuário

---
+++ Temperatura.java
@@ -0,0 +1,17 @@
+package exercicios_java;
+
+public class Temperatura {
+
+	public static void main(String[] args) {
+		
+		int valor1 = 100;
+		final double FAHRENHEIT = 32;
+		final double DIVISAO = 5/9.0;
+			
+		double C = (valor1 - FAHRENHEIT) *DIVISAO; 
+			
+		System.out.println("Tempreratura: "+C);
+		
+		
+	}
+}

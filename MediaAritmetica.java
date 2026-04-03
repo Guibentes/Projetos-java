af4ececf53c4be7126b0bbfd3e82d3a67ed20ef0
Guilherme Bentes Teixeira Xavier <142184820+Guibentes@users.noreply.github.com>
2026-04-03 20:03:17 -0300

    Media da media
    estudo

--- MediaAritmetica.java
+++ MediaAritmetica.java
@@ -9,22 +9,46 @@ public class MediaAritmetica {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		double nota1, nota2, nota3, nota4;
+		double nota1 = 8, nota2 = 9, nota3 = 7;
+		double nota4 = 4, nota5 = 5, nota6 = 6;
 		double media;
+		double media1;
 		
-		System.out.println("Digite sua nota:");
-		nota1 = scanner.nextDouble();
-		System.out.println("Digite sua nota:");
-		nota2 = scanner.nextDouble();
-		System.out.println("Digite sua nota:");
-		nota3 = scanner.nextDouble();
-		System.out.println("Digite sua nota:");
-		nota4 = scanner.nextDouble();
+		double resultado; 
+
+			System.out.println("Agora digite as notas do Primeiro semestre");
+		 	scanner.nextLine();
+		 
+		    System.out.print("Digite sua nota:");
+		    nota1 = scanner.nextDouble();
+		    System.out.print("Digite sua nota:");
+		 	nota2 = scanner.nextDouble();
+		 	System.out.print("Digite sua nota:");
+		 	nota3 = scanner.nextDouble();
+		 	
+		 	System.out.println("Agora digite as notas do segundo semestre");
+		 	scanner.nextLine();
+		 
+		 	System.out.print("Digite sua nota:");
+		 	nota4 = scanner.nextDouble();
+		 	System.out.print("Digite sua nota:");
+		 	nota5 = scanner.nextDouble();
+		 	System.out.print("Digite sua nota:");
+		 	nota6 = scanner.nextDouble();
+		 
+
+		media = (nota1 + nota2 + nota3 )/3;
 		
-		media = (nota1 + nota2 + nota3 + nota4)/4; 
+		media1 = (nota4 + nota5 + nota6)/3;
 		
-		System.out.println("Sua media foi: " + media);
+		resultado = (media + media1)/2;
+		
+		System.out.println("Sua media Total foi: " + resultado);
 		
 		scanner.close();
+		
+		
+		
 	}
 }
+
